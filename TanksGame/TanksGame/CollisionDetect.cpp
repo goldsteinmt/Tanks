60c4c29a94c7e6962a9018619b5537269c02b91e
goldsteinmt <matthew.goldstein5158@hotmail.com>
2015-04-02 17:08:12 -0400

    Matt014
    Fixed Superclass, and working on CollisionDetection

--- TanksGame/TanksGame/CollisionDetect.cpp
+++ TanksGame/TanksGame/CollisionDetect.cpp
@@ -1,3 +1,7 @@
 #include "CollisionDetect.h"
 
 // AUTHOR: Matthew Goldstein
+
+bool CollisionDetect::detectCollide(TanksObject obj1, TanksObject obj2){
+	int obj1_x, obj1_y, obj1_w, obj1_h, obj2_x, obj2_y, obj2_w, obj2_h; // x, y, width and height of both objects
+}
--- TanksGame/TanksGame/CollisionDetect.h
+++ TanksGame/TanksGame/CollisionDetect.h
@@ -1,10 +1,6 @@
 #pragma once
 
-#include "Bullets.h"
-#include "Mines.h"
-#include "Tanks.h"
-#include "AITanks.h"
-#include "Walls.h"
+#include "TanksObject.h"
 
 // AUTHOR: Matthew Goldstein
 
@@ -12,15 +8,5 @@ class CollisionDetect {
 private:
 
 public:
-	//bool checkCollide(Bullets, Bullets); // Check if a bullet collides with another bullet
-	bool checkCollide(Bullets, Walls); // Check if the bullet hit the wall
-	bool checkCollide(Bullets, AITanks); // Check if the bullet hit an AI Tank
-	bool checkCollide(Bullets, Tanks); // Check if the bullet hit the player tank
-	bool checkCollide(Bullets, Mines); // Check if the bullet hit the mine
-	bool checkCollide(AITanks, Tanks); // Check if the AI and the player hit each other
-	bool checkCollide(AITanks, AITanks); // Check if two AI Tanks hit each other
-	bool checkCollide(AITanks, Walls); // Check if the AI hits a wall
-	bool checkCollide(AITanks, Mines); // Check if the AI ran over a mine
-	bool checkCollide(Tanks, Mines); // Check if the player ran over a mine
-	bool checkCollide(Tanks, Walls); // Check if the player crashed into the wall
+	bool detectCollide(TanksObject, TanksObject); // Check if the two TanksObjects collide
 };
