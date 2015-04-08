5d622b72ef53247a038a26fac3df404ec8941f74
savanahmany <smany@student.dwc.edu>
2015-04-08 14:44:04 -0400

    savanah012
    tanks class can keep track of mines

--- TanksGame/TanksGame/Mines.h
+++ TanksGame/TanksGame/Mines.h
@@ -1,5 +1,8 @@
 #pragma once
 #include "TanksObject.h"
 ref class Mines: public TanksObject{
+private:
 
+public:
+	Mines(int, int);
 };
--- TanksGame/TanksGame/Tanks.h
+++ TanksGame/TanksGame/Tanks.h
@@ -3,10 +3,14 @@
 
 ref class Tanks: public TanksObject{
 private:
+	int num_mines;
 
 public:
 	Tanks(int, int);
 
 	void move(int key);
+	int pocket();
+	void grabMine();
+	void dropMine();
 
 };
