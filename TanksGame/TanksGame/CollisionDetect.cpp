0cc8f75feb1aee15a2c943a991b96d437e56f03d
goldsteinmt <mgoldstein@student.dwc.edu>
2015-04-27 22:18:25 -0400

    Matt037
    Fixed stupid CollisionDetect issue that I should have seen right away

--- TanksGame/TanksGame/CollisionDetect.cpp
+++ TanksGame/TanksGame/CollisionDetect.cpp
@@ -4,14 +4,14 @@
 
 bool CollisionDetect::detectCollide(TanksObject ^obj1, TanksObject ^obj2){
 	int obj1_x, obj1_y, obj1_w, obj1_h, obj2_x, obj2_y, obj2_w, obj2_h; // x, y, width and height of both objects
-	obj1_x = obj1.get_x();
-	obj1_y = obj1.get_y();
-	obj1_w = obj1.get_width();
-	obj1_h = obj1.get_height();
-	obj2_x = obj2.get_x();
-	obj2_y = obj2.get_y();
-	obj2_w = obj2.get_width();
-	obj2_h = obj2.get_height();
+	obj1_x = obj1->get_x();
+	obj1_y = obj1->get_y();
+	obj1_w = obj1->get_width();
+	obj1_h = obj1->get_height();
+	obj2_x = obj2->get_x();
+	obj2_y = obj2->get_y();
+	obj2_w = obj2->get_width();
+	obj2_h = obj2->get_height();
 
 	// check x values
 	if ((obj1_x >= obj2_x && obj1_x <= (obj2_x + obj2_w)) ||
