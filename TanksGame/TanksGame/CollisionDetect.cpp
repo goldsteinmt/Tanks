4a7a235f3ce6508059215d5a78e7bad5b952a87b
goldsteinmt <mgoldstein@student.dwc.edu>
2015-04-28 00:30:56 -0400

    Matt043
    Some collision detection work
    Made bullet look better
    Made a debug bullet for Joe (cyan for no reason)

--- TanksGame/TanksGame/CollisionDetect.cpp
+++ TanksGame/TanksGame/CollisionDetect.cpp
@@ -13,7 +13,6 @@ bool CollisionDetect::detectCollide(TanksObject ^obj1, TanksObject ^obj2){
 	obj2_w = obj2->get_width();
 	obj2_h = obj2->get_height();
 
-	/*
 	// check x values
 	if ((obj1_x >= obj2_x && obj1_x <= (obj2_x + obj2_w)) ||
 		((obj1_x + obj1_w) >= obj2_x && (obj1_x + obj1_w) <= (obj2_x + obj2_w))){
@@ -23,17 +22,28 @@ bool CollisionDetect::detectCollide(TanksObject ^obj1, TanksObject ^obj2){
 			return true; // the two objects collided
 		}
 	}
-	*/
+	
+	return false; // the two objects didn't collide
+}
 
-	for (int x1 = obj1_x; x1 < obj1_x + obj1_w; x1++){ // check obj1 x values
-		for (int x2 = obj2_x; x2 < obj2_x + obj2_w; x2++){ // check obj2 x values
-			for (int y1 = obj1_y; y1 < obj1_y + obj1_h; y1++){ // check obj1 y values
-				for (int y2 = obj2_y; y2 < obj2_y + obj2_h; y2++){ // check obj2 y values
-					if (x1 == x2 && y1 == y2){
-						return true; // the two objects collided
-					}
-				}
-			}
+bool CollisionDetect::detectCollide(TanksObject ^obj1, TanksObject ^obj2, int direction){
+	int obj1_x, obj1_y, obj1_w, obj1_h, obj2_x, obj2_y, obj2_w, obj2_h; // x, y, width and height of both objects
+	obj1_x = obj1->get_x();
+	obj1_y = obj1->get_y();
+	obj1_w = obj1->get_width();
+	obj1_h = obj1->get_height();
+	obj2_x = obj2->get_x();
+	obj2_y = obj2->get_y();
+	obj2_w = obj2->get_width();
+	obj2_h = obj2->get_height();
+
+	// check x values
+	if ((obj1_x >= obj2_x && obj1_x <= (obj2_x + obj2_w)) ||
+		((obj1_x + obj1_w) >= obj2_x && (obj1_x + obj1_w) <= (obj2_x + obj2_w))){
+		// check y values
+		if ((obj1_y >= obj2_y && obj1_y <= (obj2_y + obj2_h)) ||
+			((obj1_y + obj1_h) >= obj2_y && (obj1_y + obj1_h) <= (obj2_y + obj2_h))){
+			return true; // the two objects collided
 		}
 	}
 
--- TanksGame/TanksGame/CollisionDetect.h
+++ TanksGame/TanksGame/CollisionDetect.h
@@ -7,4 +7,5 @@
 class CollisionDetect {
 public:
 	bool detectCollide(TanksObject^, TanksObject^); // Check if the two TanksObjects collide
+	bool detectCollide(TanksObject^, TanksObject^, int); // same as above with direction
 };
